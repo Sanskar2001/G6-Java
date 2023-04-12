0342581d428f51c93d8e1ae3ba6307d2079b1ace
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-12 14:01:31 +0530

    added majorityElement

---
+++ Arrays_2/majorityElement.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+
+     int cnt=1;
+     int value=nums[0];
+        int n=nums.length;
+     for(int i=1;i<n;i++)
+     {
+         if(cnt==0)
+         {
+             value=nums[i];
+             cnt=0  ;
+         }
+
+         if(nums[i]==value)
+         {
+             cnt++;
+         }
+         else
+         cnt--;  
+     }
+         return value;
+    }
+} {
+    
+}
