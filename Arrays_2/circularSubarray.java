133a27c502cda219e9b1adb098ce4f8c95fe9fd3
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-13 00:41:01 +0530

    l3

---
+++ Arrays_2/circularSubarray.java
@@ -0,0 +1,17 @@
+ class circularSubarray {
+    
+
+    /*
+     * Given a circular array generate all the subarrays of size 3
+     */
+
+
+     public static void main(String[] args) {
+        
+
+        int []arr={1,2,3,4,5};
+
+
+        
+     }
+}
