6ac76b8362defef94adaca50bc1981c457d52390
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-19 09:35:47 +0530

    lec13

---
+++ Bitwise_Operators/chocolateSale.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class chocolateSale{
+
+
+    static int minCost(int[]arr,int k)
+    {
+        int n=arr.length;
+
+        Arrays.sort(arr,0,n);
+
+        int cost=0,varities=0;
+
+        for(int currentPrice:arr)
+        {
+            cost+=currentPrice;
+            varities+=k+1;
+
+            if(varities>=n)
+            break;
+        }
+
+        return cost;
+    }
+    public static void main(String[] args) {
+        
+
+        int[] arr={4,3,2,1};
+        int k=2;
+
+        System.out.println("total cost= "+minCost(arr, k));
+    }
+}
