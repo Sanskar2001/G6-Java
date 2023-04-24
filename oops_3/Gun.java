dde092208091d2ee7b4297842a4f3bf4c77f4717
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-24 09:46:01 +0530

    lec15

---
+++ oops_3/Gun.java
@@ -0,0 +1,42 @@
+class Gun
+{
+    private String gunName;
+    private int ammo;
+    private float calibre;
+    private int damage;
+
+    public Gun(String gunName,int ammo,float calibre,int damage)
+    {
+        this.gunName=gunName;
+        this.ammo=ammo;
+        this.calibre=calibre;
+        this.damage=damage;
+
+    }
+
+    void display()
+    {
+        System.out.println("Gunname: "+gunName);
+        System.out.println("Ammo: "+ammo);
+        System.out.println("Calibre: "+calibre);
+        System.out.println("Damage: "+damage);
+    }
+
+    // setter
+    void setAmmo(int ammo)
+    {
+        this.ammo=ammo;
+    }
+    
+    void setCalibre(int calibre)
+    {
+        this.calibre=calibre;
+    }
+
+    //getter
+    int getAmmo()
+    {
+        return this.ammo;
+    }
+}
+
