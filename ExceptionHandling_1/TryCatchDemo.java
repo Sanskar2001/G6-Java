bde7d33e1925bf70de9d77ffc9677b9b39c0b545
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-05 14:13:43 +0530

    lec19

--- ExceptionHandling_1/TryCatchDemo.java
+++ ExceptionHandling_1/TryCatchDemo.java
@@ -1,3 +1,4 @@
+import java.io.*;
 class TryCatchDemo {
 
 
@@ -14,6 +15,75 @@ class TryCatchDemo {
         {
             System.out.println("Flow will always reach here");
         }
+
+
+
+        // try{
+        //     int z=55/0;
+        //     File file = new File("filename.txt");
+        //     FileInputStream fis = new FileInputStream(file);
+        // }
+        // catch(Exception e)
+        // {
+        //     System.out.println(e);
+        // }
+
+        // try{
+        //     int z=55/2;
+        //     File file = new File("filename.txt");
+        //     FileInputStream fis = new FileInputStream(file);
+        // }
+        // catch(Exception e)
+        // {
+        //     System.out.println(e);
+        // }
+
+        // try{
+        //     int z=55/2;
+        //     File file = new File("filename.txt");
+        //     FileInputStream fis = new FileInputStream(file);
+        // }
+        // catch(ArithmeticException e)
+        // {
+        //     System.out.println(e);
+        // }
+        // catch(FileNotFoundException e)
+        // {
+        //     // System.out.println("Flow is in this catch");
+
+        //     System.out.println(e.getMessage());
+        //     e.printStackTrace();
+        // }
+
+
+
+        // nested try catch
+        try{
+            int z=55/0;
+         
+        
+            File file = new File("filename.txt");
+            try{
+                FileInputStream fis = new FileInputStream(file);
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println(e);
+            }
+            
+            
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+       
+
+
+
+       
+       
+
 
         
     }
