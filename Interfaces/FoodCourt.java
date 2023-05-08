84c97bf63b570748edc4f1b11b3681d2f5784df4
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-08 14:31:49 +0530

    lec20

---
+++ Interfaces/FoodCourt.java
@@ -0,0 +1,21 @@
+class FoodCourt {
+
+    public static void main(String[] args) {
+         
+        Restraunt restraunt=new Restraunt();
+        
+        restraunt.hireStaff();
+        restraunt.bake();
+        restraunt.bake();
+        restraunt.sellPizzas();
+        restraunt.sellPizzas();
+
+
+
+        Dominos restraunt2=new Restraunt();
+        restraunt2.hireStaff();
+        restraunt2.bake();
+        restraunt2.fireStaff();
+    }
+    
+}
---
+++ Interfaces/Restraunt.java
@@ -0,0 +1,33 @@
+class Restraunt implements Dominos{
+
+   int totalStaff;
+
+   public Restraunt()
+   {
+    totalStaff=0;
+   }
+
+   public void hireStaff()
+   {
+    System.out.println("Hired a person");
+    totalStaff++;
+   }
+
+   public void fireStaff()
+   {
+    System.out.println("Firing a person");
+    totalStaff--;
+   }
+
+   public void bake()
+   {
+    System.out.println("We bake our own bread");
+   }
+
+   public void sellPizzas()
+   {
+    System.out.println("Sold a pizza");
+   }
+    
+    
+}
