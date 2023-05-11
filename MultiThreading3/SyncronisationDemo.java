8fa313c4b7dbda0726fc44b146fd3bccc5315254
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-11 11:12:32 +0530

    lec23

--- MultiThreading3/SyncronisationDemo.java
+++ MultiThreading3/SyncronisationDemo.java
@@ -1,53 +1,61 @@
 class MyPrinter {
-    public void display(String str) {
-
-        synchronized (this) {
-            for (int i = 0; i < str.length(); i++)
-                System.out.print(str.charAt(i));
+    //    public void display(String str) {
+    //
+    //        synchronized (this) {
+    //            for (int i = 0; i < str.length(); i++)
+    //                System.out.print(str.charAt(i));
+    //        }
+    //    }
+           synchronized public void display(String str) {
+    
+                for (int i = 0; i < str.length(); i++)
+                    System.out.print(str.charAt(i));
+    
+        }
+    
+    }
+    class ThreadX extends Thread
+    {
+        MyPrinter printer;
+        public ThreadX(MyPrinter printer)
+        {
+            this.printer=printer;
+        }
+    
+        @Override
+        public void run() {
+    
+            printer.display("ThisIsABigSentence");
         }
     }
-}
-class ThreadX extends Thread
-{
-    MyPrinter printer;
-    public ThreadX(MyPrinter printer)
+    class ThreadY extends Thread
     {
-        this.printer=printer;
+        MyPrinter printer;
+    
+        public  ThreadY(MyPrinter printer)
+        {
+            this.printer=printer;
+        }
+    
+        @Override
+        public void run() {
+    
+            printer.display("World");
+        }
     }
-
-    @Override
-    public void run() {
-
-        printer.display("ThisIsABigSentence");
+    
+    
+    public class SyncDemo {
+    
+        public static void main(String[] args) {
+            MyPrinter printer=new MyPrinter();
+    
+            ThreadX threadX=new ThreadX(printer);
+            ThreadY threadY=new ThreadY(printer);
+    
+            threadX.start();
+            threadY.start();
+    
+        }
     }
-}
-class ThreadY extends Thread
-{
-    MyPrinter printer;
-
-    public  ThreadY(MyPrinter printer)
-    {
-        this.printer=printer;
-    }
-
-    @Override
-    public void run() {
-
-        printer.display("World");
-    }
-}
-
-
-public class SyncDemo {
-
-    public static void main(String[] args) {
-        MyPrinter printer=new MyPrinter();
-
-        ThreadX threadX=new ThreadX(printer);
-        ThreadY threadY=new ThreadY(printer);
-
-        threadX.start();
-        threadY.start();
-
-    }
-}
+    
