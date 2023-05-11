1192c5af062e658a23de1c99e0d44aef6e564278
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-11 11:07:35 +0530

    lec23

---
+++ MultiThreading3/SyncronisationDemo.java
@@ -0,0 +1,53 @@
+class MyPrinter {
+    public void display(String str) {
+
+        synchronized (this) {
+            for (int i = 0; i < str.length(); i++)
+                System.out.print(str.charAt(i));
+        }
+    }
+}
+class ThreadX extends Thread
+{
+    MyPrinter printer;
+    public ThreadX(MyPrinter printer)
+    {
+        this.printer=printer;
+    }
+
+    @Override
+    public void run() {
+
+        printer.display("ThisIsABigSentence");
+    }
+}
+class ThreadY extends Thread
+{
+    MyPrinter printer;
+
+    public  ThreadY(MyPrinter printer)
+    {
+        this.printer=printer;
+    }
+
+    @Override
+    public void run() {
+
+        printer.display("World");
+    }
+}
+
+
+public class SyncDemo {
+
+    public static void main(String[] args) {
+        MyPrinter printer=new MyPrinter();
+
+        ThreadX threadX=new ThreadX(printer);
+        ThreadY threadY=new ThreadY(printer);
+
+        threadX.start();
+        threadY.start();
+
+    }
+}
