2262a3d085137f9334cd9791d26d86c8fc42b924
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-31 10:16:32 +0530

    lec36

---
+++ PracticeQPS/DuplicateElements.java
@@ -0,0 +1,21 @@
+class DuplicateElements {
+    
+
+    static boolean checkDuplicate(int[]arr)
+    {
+        int n=arr.length;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,2};
+
+
+        if(checkDuplicate(arr))
+        System.out.println("Duplicate element exsits");
+        else
+        System.out.println("Does'nt exists");
+    }
+}
---
+++ PracticeQPS/TripletSumZero.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	static public boolean findTriplets(int arr[] , int n)
+    {
+        //add code here.
+        
+        HashSet<Integer>set=new HashSet<Integer>();
+        
+        for(int i=0;i<=n-2;i++)
+        {
+            for(int j=i+1;j<=n-1;j++)
+            {
+                int sum=arr[i]+arr[j];
+                
+                if(set.contains(-sum))
+                return true;
+            }
+            
+            set.add(arr[i]);
+        }
+        
+        return false;
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int[]arr={1,0,2,-3,5};
+
+        if(findTriplets(arr, arr.length))
+        System.out.println("triplet with sum 0 exsists");
+        else
+        System.out.println("Such triplet not found");
+
+    }
+}
